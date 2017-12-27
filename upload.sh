a0af8079a39a6fe2f2da08cb152916312ef228ea
Latinist <antonromanov@list.ru>
2017-12-27 13:12:18 +0300

    Starter Maven Project

---
+++ upload.sh
@@ -0,0 +1,7 @@
+git add -u
+git add .
+git tag -a v1.0 -m 'STARTER MAVEN PROJECT'
+git commit -a -m "Starter Maven Project"
+git push -u origin master
+git status
+read -p "Press any key to continue... " -n1 -s
