4962db00ce6237cdb6ca9f14fb6650c8e079652c
Latinist <antonromanov@list.ru>
2017-12-27 14:33:29 +0300

    Real Workin SPRING Hello World Example

--- hellomaven/src/main/java/ru/easyjava/maven/App.java
+++ hellomaven/src/main/java/ru/easyjava/maven/App.java
@@ -1,13 +1,14 @@
 package ru.easyjava.maven;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"SpringBeans.xml");
+
+		HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+		obj.printHello();
+	}
 }
